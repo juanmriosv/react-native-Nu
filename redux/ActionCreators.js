66430c0878adf24d719c53de70905d2a2609276a
Juan Manuel Rios Villanueva <juanma@theatico.com>
2021-08-05 23:49:18 -0400

    animation and gestures

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -168,6 +168,11 @@ export const postFavorite = campsiteId => dispatch => {
     }, 2000);
 };
 
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+});
+
 export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
